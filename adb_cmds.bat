09995773e7615b7baab8bb226b855ff962978f77
Alec Wong <awongfb@fb.com>
2021-10-13 23:39:27 -0700

    update

--- adb_cmds.bat
+++ adb_cmds.bat
@@ -1,5 +1,5 @@
 adb wait-for-device && adb root && adb remount 
-@REM adb shell mfg_tool mcu raw set dev
+adb shell mfg_tool mcu raw set dev
 adb shell mfg_tool mcu raw set w ConfigHallInactiveOverride 1
 adb shell mfg_tool mcu reset
 adb shell mfg_tool mcu raw captouch force-streaming
